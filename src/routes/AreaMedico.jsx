c429c6aa6c01ff3aa3796285e84a884fb0830324
matheus machado caposse <matheus.caposse02@gmail.com>
2025-04-29 12:27:08 -0300

    fazendo a logica para enviar a call para a vm

--- src/routes/AreaMedico.jsx
+++ src/routes/AreaMedico.jsx
@@ -1,30 +1,42 @@
 import React, { useState } from 'react';
-import '../styles/AreaMedicoStyled.css'
+import '../styles/AreaMedicoStyled.css';
 
 const AreaMedico = () => {
-
-  // Usando um estado para controlar cada botão "Chamar" individualmente
   const [chamados, setChamados] = useState({
-    1: false, // Para o primeiro botão
-    2: false, // Para o segundo botão
-    3: false, // Para o terceiro botão
-    4: false, // Para o quarto botão
-    5: false, // Para o quinto botão
-    6: false  // Para o sexto botão
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false,
+    6: false
   });
 
-  const handleChamarClick = (id) => {
+  const handleChamarClick = async (id) => {
     setChamados(prevState => ({
       ...prevState,
-      [id]: true, // Altera o estado do botão específico
+      [id]: true,
     }));
-  }
+
+    try {
+      const response = await fetch('http://34.42.65.153:5000/acionar', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id }) // Você pode ajustar o payload se necessário
+      });
+
+      const data = await response.json();
+      console.log('Resposta da VM:', data);
+    } catch (error) {
+      console.error('Erro ao chamar a VM:', error);
+    }
+  };
 
   return (
     <>
       <h2>Lista de espera: </h2>
-
-      <div className="container-fila">
+      <div div className="container-fila">
         <form className="grave">
           <h3>Nome: Thiago Almeida</h3>
           <p>Idade: 11</p>
@@ -33,31 +45,23 @@ const AreaMedico = () => {
           <p>Motivo: Suspeita de tumor</p>
           <p>Gravidade: Grave</p>
           <hr />
-          <button 
-            type='button' 
-            className='chamar' 
-            onClick={() => handleChamarClick(1)}
-          >
+          <button type='button' className='chamar' onClick={() => handleChamarClick(1)}>
             {chamados[1] ? 'Chamado' : 'Chamar'}
-          </button> 
+          </button>
           <button>Concluido</button>
         </form>
-        
+
         <form className="moderada">
           <h3>Nome: Ricardo Neves</h3>
           <p>Idade: 9</p>
           <p>CPF: 512.841.908-33</p>
           <p>sintomas: Dores no peito</p>
-          <p>Motivo: N/A </p>
+          <p>Motivo: N/A</p>
           <p>Gravidade: Moderada</p>
           <hr />
-          <button 
-            type='button' 
-            className='chamar' 
-            onClick={() => handleChamarClick(2)}
-          >
+          <button type='button' className='chamar' onClick={() => handleChamarClick(2)}>
             {chamados[2] ? 'Chamado' : 'Chamar'}
-          </button> 
+          </button>
           <button>Concluido</button>
         </form>
 
@@ -66,34 +70,26 @@ const AreaMedico = () => {
           <p>Idade: 10</p>
           <p>CPF: 401.909.575-33</p>
           <p>sintomas: Febre e dor de garganta</p>
-          <p>Motivo: Bacteria </p>
+          <p>Motivo: Bactéria</p>
           <p>Gravidade: Leve</p>
           <hr />
-          <button 
-            type='button' 
-            className='chamar' 
-            onClick={() => handleChamarClick(3)}
-          >
+          <button type='button' className='chamar' onClick={() => handleChamarClick(3)}>
             {chamados[3] ? 'Chamado' : 'Chamar'}
-          </button> 
+          </button>
           <button>Concluido</button>
-        </form> 
+        </form>
 
         <form className="moderada">
           <h3>Nome: Antonio Nunes</h3>
           <p>Idade: 12</p>
           <p>CPF: 512.841.908-33</p>
           <p>sintomas: Machucado na perna</p>
-          <p>Motivo: Cão com suspeita de raiva </p>
+          <p>Motivo: Cão com suspeita de raiva</p>
           <p>Gravidade: Moderada</p>
           <hr />
-          <button 
-            type='button' 
-            className='chamar' 
-            onClick={() => handleChamarClick(4)}
-          >
+          <button type='button' className='chamar' onClick={() => handleChamarClick(4)}>
             {chamados[4] ? 'Chamado' : 'Chamar'}
-          </button> 
+          </button>
           <button>Concluido</button>
         </form>
 
@@ -102,16 +98,12 @@ const AreaMedico = () => {
           <p>Idade: 13</p>
           <p>CPF: 542.334.708-50</p>
           <p>sintomas: Dores nas costas</p>
-          <p>Motivo: Queda </p>
+          <p>Motivo: Queda</p>
           <p>Gravidade: Leve</p>
           <hr />
-          <button 
-            type='button' 
-            className='chamar' 
-            onClick={() => handleChamarClick(5)}
-          >
+          <button type='button' className='chamar' onClick={() => handleChamarClick(5)}>
             {chamados[5] ? 'Chamado' : 'Chamar'}
-          </button> 
+          </button>
           <button>Concluido</button>
         </form>
 
@@ -120,23 +112,17 @@ const AreaMedico = () => {
           <p>Idade: 10</p>
           <p>CPF: 545.278.408-80</p>
           <p>sintomas: Febre forte</p>
-          <p>Motivo: N/A </p>
+          <p>Motivo: N/A</p>
           <p>Gravidade: Leve</p>
           <hr />
-          <button 
-            type='button' 
-            className='chamar' 
-            onClick={() => handleChamarClick(6)}
-          >
+          <button type='button' className='chamar' onClick={() => handleChamarClick(6)}>
             {chamados[6] ? 'Chamado' : 'Chamar'}
-          </button> 
+          </button>
           <button>Concluido</button>
         </form>
-
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default AreaMedico;
